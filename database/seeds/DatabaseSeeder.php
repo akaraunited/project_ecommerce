6dbb610517409184e0463e759d613fe39c580e6c
Phov Thea <phthea.it@gmail.com>
2020-03-28 21:16:08 +0700

    edit seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(CategoriesTableSeeder::class);
+        $this->call(ProductTableSeeder::class);
+        $this->call(ImageTableSeeder::class);
     }
 }
